ab6410afb21d580fc354cba31cfc91e453327406
Zaharia Laurentiu Jr Marius <zaharialorenzo@gmail.com>
2022-04-27 16:39:42 +0200

    Changes by raky1

--- analysis/language_understanding.py
+++ analysis/language_understanding.py
@@ -3,10 +3,11 @@ import spacy
 
 def parse_sentence(sentence):
     nlp = spacy.load('en_core_web_trf')
-    doc = nlp(sentence)
-    print()
-    for chunk in doc.noun_chunks:
-        print(chunk.text, chunk.root.text, chunk.root.dep_,
-              chunk.root.head.text)
+    doc = nlp(sentence, disable=['ner', 'lemmatizer', 'textcat'])
 
-    displacy.serve(doc, host='127.0.0.1', style='dep')
+    labels = {'attr', 'conj', 'ROOT', 'dobj', 'iobj', 'pobj'}
+    ingredients = {np.text
+                   for nc in doc.noun_chunks
+                   for np in [nc, doc[nc.root.left_edge.i: nc.root.right_edge.i + 1]]}
+
+    return ingredients
--- db/potion_dictionary.py
+++ db/potion_dictionary.py
@@ -1,5 +1,5 @@
 potions = {"polyjuice": ['fluxweed', 'knotgrass', 'lacewing flies', 'leeches', 'horn of bicorn', 'boomslang skin',
-                         ['piece of person you are turning into', 'hair', 'nail', 'skin']],
+                         'a piece of the person you are turning into', 'hair', 'nail', 'skin'],
            "armadillo bile mixture": ['bat wings', 'armadillo bile', 'wormwood', 'amorentia', 'acromantula venom',
                                       'aconite', 'water'],
            "animagus": ['mandrake leaf', 'drinker\'s hair', 'dew', 'death\'s-head hawk chrysalis']}
