de318d6740e57b8ea272e1215da2d5b54cdac288
Zaharia Laurentiu Jr Marius <zaharialorenzo@gmail.com>
2022-04-27 16:39:37 +0200

    Changes by Rachele

--- db/potion_dictionary.py
+++ db/potion_dictionary.py
@@ -3,3 +3,21 @@ potions = {"polyjuice": ['fluxweed', 'knotgrass', 'lacewing flies', 'leeches', '
            "armadillo bile mixture": ['bat wings', 'armadillo bile', 'wormwood', 'amorentia', 'acromantula venom',
                                       'aconite', 'water'],
            "animagus": ['mandrake leaf', 'drinker\'s hair', 'dew', 'death\'s-head hawk chrysalis']}
+
+ingredients = ['fluxweed', 'knotgrass', 'lacewing flies', 'leeches', 'horn of bicorn', 'boomslang skin',
+               'a piece of the person you are turning into', 'hair', 'nail', 'skin', 'bat wings', 'armadillo bile',
+               'wormwood', 'amorentia', 'acromantula venom', 'aconite', 'water', 'mandrake leaf', 'drinker\'s hair',
+               'dew', 'death\'s-head hawk chrysalis', 'newt spleens', 'bananas', 'an orange snake', 'a green leaf',
+               'angel\'s Trumpet', 'valerian sprigs', 'dittany', 'baneberry', 'cheese', 'death- cap', 'pumpkin juice',
+               'unicorn horn', 'caterpillars', 'orange juice', 'extract of gurdyroot', 'dragon dung', 'salamander blood',
+               ' flobberworm mucus']
+
+
+
+#FRASI
+#Yes, it is easy to see that nearly six years of magical education have (not) been wasted on you, Potter.
+#I would expect nothing more sophisticated from you
+#
+#Well, it may have escaped your notice, but *** IS NOT AN INGREDIENT FOR THIS POTION.
+#at the end of this lesson we will feed a few drops of this potion to your toad and see what happens. Perhaps that will encourage you to do it properly.
+# Well, it may have escaped your notice, but *** IS NOT AN INGREDIENT FOR THIS POTION.
---
+++ dm/context_model.py
@@ -0,0 +1,49 @@
+import random
+from db.potion_dictionary import potions, ingredients
+from dm import frames
+from analysis import language_understanding as lu
+import pandas as pd
+
+
+class Memory:
+    def __init__(self) -> None:
+        super().__init__()
+        self._data_frame = pd.DataFrame(columns=['sentence', 'right', 'wrong', 'complete'])
+
+    def get_data_frame(self):
+        return self._data_frame
+
+    def update(self, sentence: str, right: int, wrong: int, complete: float):
+        self._data_frame.loc[self._data_frame.size] = [sentence, right, wrong, complete]
+
+
+class DialogContextModel:
+    memory = Memory()
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.context = None
+
+    def __call__(self, *args, **kwargs):
+        potion = random.choice(list(potions))
+        ingredients = potions[potion]
+
+        if potion == 'polyjuice':
+            self.context = frames.PolyjuiceFrame()
+        elif potion == 'armadillo bile mixture':
+            self.context = frames.ArmadilloBileMixtureFrame()
+        elif potion == 'animagus':
+            self.context = frames.AnimagusFrame()
+
+    def process_input(self, sentence):
+        subtrees = lu.parse_sentence(sentence)
+        right = 0
+        wrong = 0
+        for tree in subtrees:
+            if tree in potions[self.context.name]:
+                right += 1
+                self.context.set_ingredient(frames.IngredientFrame(str(tree)))
+            elif tree in ingredients:
+                wrong += 1
+        print(self.context.show_attributes())
+        self.memory.update(sentence, right, wrong, self.context.is_complete())
